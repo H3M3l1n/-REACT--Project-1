062422e338f7ebc4f9385c124bbe89a86f8ff5d6
Andrew <hemelin23@gmail.com>
2025-04-06 11:03:42 +0300

    delete unused animation

--- src/components/customlink/CustomLink.jsx
+++ src/components/customlink/CustomLink.jsx
@@ -1,38 +1,20 @@
-import { Link, useMatch } from "react-router-dom";
-import { motion } from "framer-motion";
+import { Link, useMatch } from 'react-router-dom';
+
+import './customlink.sass';
 
 const CustomLink = ({ children, to, handler }) => {
-  const match = useMatch({
-    path: to,
-    end: to.length === 1,
-  });
+    const match = useMatch({
+        path: to,
+        end: to.length === 1,
+    });
 
-  const navLinkVariants = {
-    inactive: {
-      backgroundColor: "transparent",
-      textDecoration: "unset",
-      textDecorationColor: "#0081c2",
-    },
-    active: {
-      backgroundColor: "#0081c2",
-      cursor: "default",
-    },
-    hover: { textDecoration: "underline", textDecorationColor: "#0081c2" },
-  };
-
-  return (
-    <Link to={to} className="nav-link" onClick={handler}>
-      <motion.span
-        className="nav-link-span"
-        variants={navLinkVariants}
-        initial={"inactive"}
-        animate={match ? "active" : "inactive"}
-        whileHover={"hover"}
-      >
-        {children}
-      </motion.span>
-    </Link>
-  );
+    return (
+        <Link to={to} className="nav-link" onClick={handler}>
+            <span className={`customlink ${match ? 'customlink-active' : ''}`}>
+                {children}
+            </span>
+        </Link>
+    );
 };
 
 export default CustomLink;
